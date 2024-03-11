c5ac4ca16c01530e88f0f4416f077aaaff1f7bfb
Sarah Frainer <sbfrainer@gmail.com>
2024-03-11 14:47:30 -0300

    Exercicio2

--- Semana8.2/script.js
+++ Semana8.2/script.js
@@ -4,3 +4,24 @@ console.log (tit);
 
 tit.style.backgroundColor = 'blue';
 
+var tituloprinc = document.querySelector ('.titulo1');
+
+console.log (tituloprinc)
+
+tituloprinc.textContent = 'Esse é o novo título';
+
+
+var paragrafoprinc = document.querySelector ('.paragrafo1');
+
+console.log (paragrafoprinc)
+
+paragrafoprinc.textContent = 'Esse é o novo parágrafo';
+
+
+var itenslista = document.querySelectorAll ('.item');
+
+console.log (itenslista);
+
+itenslista[0].textContent = 'Item 1 atualizado;';
+itenslista[1].textContent = 'Item 2 atualizado;';
+itenslista[2].textContent = 'Item 3 atualizado;';
