2add528e38e52e54fd006bca4589b47f12830139
Sarah Frainer <sbfrainer@gmail.com>
2024-03-12 16:20:14 -0300

    Botão.retornarvalor

--- exercíciosSemana5/Exerciciostrello/script.js
+++ exercíciosSemana5/Exerciciostrello/script.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const resposta = document.getElementById('valor');
+    let carrinho = [];
 
     function consultarPreco() {
         const resposta2 = resposta.value.toLowerCase();
@@ -26,37 +27,47 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    
-    document.getElementById('consulta').addEventListener('click', consultarPreco);
-
-    let carrinho = [];
-
-    function adicionarCarrinho (){
+    function adicionarCarrinho() {
         const resposta4 = resposta.value.toLowerCase();
-        const prodArray = valCarrinho (resposta4);
+        const prodArray = valCarrinho(resposta4);
         carrinho.push(prodArray);
-        console.log (carrinho);
+        console.log(carrinho);
     }
 
-    function valCarrinho (resposta4){
+    function valCarrinho(resposta4) {
         switch (resposta4) {
             case 'banana':
             case '1':
-                return 'banana';
+                return { nome: 'banana', preço: 'R$1,99' };
 
             case 'mirtilo':
             case '2':
-                return 'mirtilo';
+                return { nome: 'mirtilo', preço: 'R$10,50' };
 
             case 'morango':
             case '3':
-                return 'morango';
+                return { nome: 'morango', preço: 'R$4,99' };
 
             default:
                 return 'Produto não cadastrado';
-    }
+        }
     }
 
+    function consultarValor() {
+        let total = 0;
+        if (carrinho.length > 0) {
+        carrinho.forEach(item => {
+            const preço = parseFloat(item.preço.replace('R$', '').replace(',', '.'));
+            total += preço;
+        });
+        alert("O total da compra é: R$" + total.toFixed(2));
+    }
+        else {
+        alert ("O carrinho está vazio!")
+        }
+    }
+
+    document.getElementById('consulta').addEventListener('click', consultarPreco);
     document.getElementById('compra').addEventListener('click', adicionarCarrinho);
-
+    document.getElementById('valorCompra').addEventListener('click', consultarValor);
 });
