d9ee8d3b9cd4a07dd76ad2e9f5a3227780c5b4eb
Sarah Frainer <sbfrainer@gmail.com>
2024-03-28 14:18:12 -0300

    ex4S10

--- projeto-react-teste/src/components/card.jsx
+++ projeto-react-teste/src/components/card.jsx
@@ -1,8 +1,13 @@
 export default function Card (props) {
     return (
         <>
-        
-        
+        <h1><b>{props.titulo} - {props.cidade.estado}</b></h1>
+        <p><b>Por: {props.criador}</b></p>
+        <p>{props.duracao}</p>
+        <p>{props.trajeto}</p>
+        <div>{props.dificuldade}</div>
+        <div>{props.tipoDeTrilha}</div>
+        <img link="{props.link}"></img>
         </>
     )
 }
