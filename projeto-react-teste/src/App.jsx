8dd59f53db6cbc696ddd662dcdccf565ee796379
Sarah Frainer <sbfrainer@gmail.com>
2024-03-28 21:25:41 -0300

    Ex5S10

--- projeto-react-teste/src/App.jsx
+++ projeto-react-teste/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import Card from "./components/Card"
 
-function App() {
+export default function App() {
   return (
-      <div className = "tituloPagina">
+      <>
       <h1>Explore trilhas incríveis</h1>
-      </div>
+      <Card titulo="Trilha do macaco" cidadeEstado="Florianópolis - SC" criador="Sarah" duracao="2 horas" trajeto="6km" dificuldade="intermediário" tipoDeTrilha="Caminhada" link="https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+      <Card titulo="Trilha do Damião" cidadeEstado="Bombinhas - SC" criador="Sarah" duracao="1 hora" trajeto="3km" dificuldade="iniciante" tipoDeTrilha="Caminhada" link="https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+      <Card titulo="Trilha do Canoa" cidadeEstado="Piçarras - SC" criador="Sarah" duracao="3,5 horas" trajeto="8km" dificuldade="avançado" tipoDeTrilha="Caminhada" link="https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+      </>
   )
 }
-
-export default App
--- projeto-react-teste/src/components/card.jsx
+++ projeto-react-teste/src/components/card.jsx
@@ -1,13 +1,13 @@
 export default function Card (props) {
     return (
         <>
-        <h1><b>{props.titulo} - {props.cidade.estado}</b></h1>
+        <h1><b>{props.titulo} - {props.cidadeEstado}</b></h1>
         <p><b>Por: {props.criador}</b></p>
         <p>{props.duracao}</p>
         <p>{props.trajeto}</p>
         <div>{props.dificuldade}</div>
         <div>{props.tipoDeTrilha}</div>
-        <img link="{props.link}"></img>
+        <img width="200px" src={props.link}></img>
         </>
     )
 }
