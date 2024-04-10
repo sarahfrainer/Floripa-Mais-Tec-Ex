0cdafac109e704c95f9682f30be0eb44ac47faf9
Sarah Frainer <sbfrainer@gmail.com>
2024-04-10 14:33:54 -0300

    Ex1S12

--- projeto-react-teste/src/main.jsx
+++ projeto-react-teste/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import routers from './routers/router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <RouterProvider router={router}>
     <App />
-  </React.StrictMode>,
+  </RouterProvider>,
 )
---
+++ projeto-react-teste/src/routers/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routers = createBrowserRouter ([
+    {
+        path: "/",
+        element:
+    },
+    {
+        path: "/",
+        element:
+    },
+    {
+        path: "/",
+        element:
+    },
+])
+
+export default routers
